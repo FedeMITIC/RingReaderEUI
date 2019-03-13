505f2475346cf796a94e66da0ab84b99ca5882cd
Federico Macchi <federico.macchi@mail.polimi.it>
2019-03-13 16:12:15 +0200

    Created entry point

--- main.py
+++ main.py
@@ -1,15 +1,62 @@
-from time import sleep
-from picamera import PiCamera
+#!/usr/bin/env python3
+
+"""
+File: main.py
+
+Entry point of the application:
+- reads the API Key from a file external to the repo and saves it in the global variable API_KEY
+- captures the image from the PiCamera
+- extracts the text from the image
+- transforms the text in speech
+- plays the resulting audio file
+
+Author:     Federico Macchi
+Date:	    26/02/2019
+Version     0.1.0
+License:    MIT
+"""
+
+from utility import *
+# from detect_text import image_to_text
+from tts import text_to_speech
 
 
 def main():
-    camera = PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    # Camera warm-up time
-    sleep(2)
-    camera.capture('foo.jpg')
+    # while True:
+    #     if button_pressed():
+    #         start()
+    #     else:
+    #         pass
+    start()
+
+
+def start():
+    # Define the params to capture the image
+    params = []
+    # Capture the image using the camera
+    image = capture_image(parameters=params)
+    # Extract the text from the image
+    # text = image_to_text(API_KEY, image)
+    text = 'Some random text that will be read aloud from the Raspberry Pi\'s speakers'
+    # Transform the text into speech
+    audio = text_to_speech(api_key=API_KEY, text=text)
+    # Play audio
+    audio_path = 'audio.ogg'
+    play_audio(path=audio_path)
+
+
+def setup():
+    # Get the API key
+    try:
+        f = open('../API.txt', 'r')
+        global API_KEY
+        API_KEY = f.read()
+        f.close()
+    except FileNotFoundError:
+        print('Impossible to load API Key')
+        exit(1)
 
 
 if __name__ == "__main__":
+    setup()
     main()
