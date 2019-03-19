510968a5aaf330e12433fe7cd974dc657383e528
Federico Macchi <federico.macchi@mail.polimi.it>
2019-03-19 16:57:58 +0200

    Added button functionality

--- utility.py
+++ utility.py
@@ -3,6 +3,7 @@ File: utility.py
 
 Contains various utility functions:
 - button_pressed(): detects if the user pressed a button connected to the PI
+- button_setup(): sets up the button [see https://www.makeuseof.com/tag/add-button-raspberry-pi-project]
 - capture_image(parameters): captures the image from the Pi Camera
 - play_audio(path): loads the audio file stored in 'path' and plays it
 
@@ -12,10 +13,24 @@ Version     0.1.0
 License:    MIT
 """
 import pygame
+import RPi.GPIO as GPIO
 
 
 def button_pressed():
-    return True
+    # if not is_button_setup:
+    #    print('First ensure that the button is properly setted-up.')
+    #    exit(1)
+    return GPIO.input(button_pin)
+
+
+def button_setup():
+    global is_button_setup
+    is_button_setup = False
+    GPIO.setmode(GPIO.BOARD)
+    global button_pin
+    button_pin = 16
+    GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    is_button_setup = True
 
 
 def capture_image(parameters):
