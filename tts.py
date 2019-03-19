0805913dc8848385ac04c7036f54c3c672cbba07
Federico Macchi <federico.macchi@mail.polimi.it>
2019-03-19 16:35:34 +0200

    Code refactor

--- tts.py
+++ tts.py
@@ -39,35 +39,25 @@ Date:	    26/02/2019
 Version     0.1.0
 License:    MIT
 """
+from api import *
 
-import requests
 
-
-def text_to_speech(api_key, text):
+def text_to_speech(api_key, text, path):
     # Append the API Key to the endpoint to authenticate with the APIs
     endpoint = 'https://texttospeech.googleapis.com/v1/text:synthesize?key=' + str(api_key)
-    print(endpoint)
-    # API call
-    # with open('output.ogg', 'wb') as out:
-    #    out.write(response.audio_content)
-    #    print('Audio content written to file "output.mp3"')
+    result = False
+    try:
+        result = synthesize_text_api(endpoint=endpoint, data_to_send=text, file_path=path)
+    except APIError:
+        print('API call failed.')
+        result = False
+    return result
 
 
+# Used only to test if the API works, will be removed.
 if __name__ == '__main__':
     print('DEBUG')
     text = "Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say that they were perfectly normal, " \
            "thank you very much. They were the last people you'd expect to be involved in anything strange or " \
-           "mysterious, because they just didn't hold with such nonsense. Mr. Dursley was the director of a firm " \
-           "called Grunnings, which made drills. He was a big, beefy man with hardly any neck, although he did have a " \
-           "very large mustache. Mrs. Dursley was thin and blonde and had nearly twice the usual amount of neck, " \
-           "which came in very useful as she spent so much of her time craning over garden fences, spying on the  " \
-           "neighbors. The Dursleys had a small son called Dudley and in their opinion there was no finer boy " \
-           "anywhere. The Dursleys had everything they wanted, but they also had a secret, and their greatest fear " \
-           "was that somebody would discover it. They didn't think they could bear it if anyone found out about the " \
-           "Potters. Mrs. Potter was Mrs. Dursley's sister, but they hadn't met for several years; in fact, " \
-           "Mrs. Dursley pretended she didn't have a sister, because her sister and her good-for-nothing husband were " \
-           "as unDursleyish as it was possible to be. The Dursleys shuddered to think what the neighbors would say if " \
-           "the Potters arrived in the street. The Dursleys knew that the Potters had a small son, too, but they had " \
-           "never even seen him. This boy was another good reason for keeping the Potters away; they didn't want " \
-           "Dudley mixing with a child like that. "
-    text_to_speech(api_key=open('../API.txt', 'r').read(), text='daw')
+           "mysterious, because they just didn't hold with such nonsense."
+    text_to_speech(api_key=open('../API.txt', 'r').read(), text=text, path='output.ogg')
